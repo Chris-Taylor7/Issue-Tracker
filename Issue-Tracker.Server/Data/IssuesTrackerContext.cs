74295627dfba804cbbfdf6781239dbc1a4733b6f
Taylo5ce <chtaylor270@gmail.com>
2024-05-04 22:37:09 -0400

    inital commit

--- Issue-Tracker.Server/Data/IssuesTrackerContext.cs
+++ Issue-Tracker.Server/Data/IssuesTrackerContext.cs
@@ -2,10 +2,23 @@ using Microsoft.EntityFrameworkCore;
 
 public class IssueTrackerContext : DbContext
 {
-    public IssueTrackerContext (DbContextOptions<IssueTrackerContext> IssueTrackerContext ) : base(IssueTrackerContext )
+    public IssueTrackerContext(DbContextOptions<IssueTrackerContext> options) : base(options)
     {
+        // Your constructor logic here
     }
 
     // Define your DbSet properties here
     public DbSet<Issue> Issues { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Issue>()
+            .HasKey(e => e.IssueId); // Set IssueId as the primary key
+
+        modelBuilder.Entity<Issue>()
+            .Property(e => e.IssueId)
+            .ValueGeneratedOnAdd(); // Set IssueId to be auto-generated when adding new issues
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
